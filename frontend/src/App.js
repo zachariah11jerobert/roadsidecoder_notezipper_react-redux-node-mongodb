4e34d8bc73d13e3649a4135aefb936ccafe5bf3e
unknown <zachariah11jerobert@gmail.com>
2022-03-16 10:10:13 +0530

    modified mistake

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,12 @@ import "./App.css";
 
 function App() {
   const hello = 1;
-  return;
-  <>
-    <div className="App">Hello world{1}</div>; ello = 1;
-    <div className="App">Hello world 2</div>
-  </>;
+  return (
+    <>
+      <div className="App">Hello world{1}</div>; ello = 1;
+      <div className="App">Hello world 2</div>
+    </>
+  );
 }
 
 export default App;
