f14ebbdd5418052a70864f78d31e0da47fd2d8f9
unknown <zachariah11jerobert@gmail.com>
2022-03-16 09:52:36 +0530

    Initial mern study & frontend folder inserted

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const notes = require("./data/notes");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+app.get("/", (req, res) => {
+  console.log(req);
+  res.send("API is running...");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((n) => n._id === req.params.id);
+  res.json(note);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server startted on PORT ${PORT}`));
