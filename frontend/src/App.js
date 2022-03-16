1c15aa30299a2154a683a86c566cdcf947b765bc
unknown <zachariah11jerobert@gmail.com>
2022-03-16 10:09:10 +0530

    basics od jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const hello = 1;
+  return;
+  <>
+    <div className="App">Hello world{1}</div>; ello = 1;
+    <div className="App">Hello world 2</div>
+  </>;
 }
 
 export default App;
