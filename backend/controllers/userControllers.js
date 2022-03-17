c87d03a1bab75a62677a9d9af12e26599888f6b8
unknown <zachariah11jerobert@gmail.com>
2022-03-17 08:33:46 +0530

    done backend code

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,53 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const registerUser = async (req, res) => {
+  const { name, email, password, pic } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Already Exists");
+  }
+
+  const user = User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Error");
+  }
+};
+
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid Email or Pasword!");
+  }
+});
+
+module.exports = { registerUser,authUser };
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const notes = require("./data/notes");
 const dotenv = require("dotenv");
 
+const { userRoutes } = require("./routes/userRoutes");
+
 const app = express();
 dotenv.config();
+connectDB();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log(req);
@@ -14,10 +18,7 @@ app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
 
-app.get("/api/notes/:id", (req, res) => {
-  const note = notes.find((n) => n._id === req.params.id);
-  res.json(note);
-});
+app.use("/api/notes", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
